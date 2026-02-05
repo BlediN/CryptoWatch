9119a42fb6aa1cf13463b4d7baa732d255fe56e7
Bledar N <bledar.n@outlook.com>
2026-02-05 02:12:18 -0500

    fixing routes

--- crypto-watch/src/App.jsx
+++ crypto-watch/src/App.jsx
@@ -1,17 +1,15 @@
 import './styles.css'
 import Dashboard from './components/Dashboard'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import CoinDetail from './components/CoinDetail'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/coin/:id" element={<CoinDetail />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/coin/:id" element={<CoinDetail />} />
+    </Routes>
   )
 }
 
